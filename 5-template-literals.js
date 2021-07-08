9e2451141dbce6de6dfdf0fae0c8f6e1994f2c27
marilia <marilia.pacifico@gmail.com>
2021-07-08 15:48:28 -0300

    add assignment 5

---
+++ 5-template-literals.js
@@ -0,0 +1,10 @@
+/* assignment 5
+recreate the "description" variable from the last assignment, this time using the template literal syntax */ 
+
+const country = "Brazil";
+const continent = "South America";
+const population = 211;
+const language = "portuguese";
+const description = `${country} is in ${continent} and its ${population} million people speak ${language}.`;
+
+// I used `` to declare the string, not having to worry about the text formating :D 
