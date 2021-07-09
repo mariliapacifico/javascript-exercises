803c75cf7b06921ec8fc04a943de4ccdeb01d83b
marilia <marilia.pacifico@gmail.com>
2021-07-09 15:52:59 -0300

    add assignment 7

---
+++ 7-type-conversion-and-coercion.js
@@ -0,0 +1,9 @@
+/* assignment 7
+- predict the result of these 5 operations without executing them
+- execute the operations to check if you were right */
+
+console.log("9"-"5"); // 4 (number)
+console.log("19" - "13" + "17"); // "617" (string)
+console.log("19 "- "13" + 17); // 23 (number)
+console.log("123" < 57); // false (boolean)
+console.log(5 + 6 + "4" + 9 - 4 - 2); // "1143" (string)
