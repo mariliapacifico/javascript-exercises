bcd225d267f2b73b0f28697f8eafec93a51e2808
marilia <marilia.pacifico@gmail.com>
2021-08-02 14:24:12 -0300

    add assignment 7

---
+++ 02 - Fundamentals - Part 2/07-introduction-to-objects.js
@@ -0,0 +1,10 @@
+/* assignment 7
+- create an object called "myCountry" for a country of your choice, containing properties "country", "capital", "language", "population" and "neighbours" (an array used in previous assignments) */
+
+const myCountry = {
+    country: "Brazil",
+    capital: "Brasilia",
+    language: "Portuguese",
+    population: 211,
+    neighbours: ["Colombia", "Argentina", "Peru"]
+};
